18c29424a6481cfa42576eaf5383b2a4207feccb
ElCubiko <54866672+ElCubiko@users.noreply.github.com>
2020-02-17 22:15:27 +0100

    Creations des routes /, /entreprises, /formations, /stages/{id} avec leurs vues respectives

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -8,12 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
     /**
-     * @Route("/pro/stage", name="pro_stage")
+     * @Route("/", name="pro_stage_acceuil")
      */
     public function index()
     {
-        return $this->render('pro_stage/index.html.twig', [
-            'controller_name' => 'ProStageController',
-        ]);
+        return $this->render('pro_stage/index.html.twig');
     }
+
+    /**
+     * @Route("/entreprises", name="pro_stage_entreprises")
+     */
+    public function showEntreprises()
+    {
+        return $this->render('pro_stage/entreprises.html.twig');
+    }
+
+    /**
+     * @Route("/formations", name="pro_stage_formations")
+     */
+    public function showFormations()
+    {
+        return $this->render('pro_stage/formations.html.twig');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="pro_stage_details_stage")
+     */
+    public function showStageDetails($id)
+    {
+        return $this->render('pro_stage/stageDetails.html.twig', ['id' => $id]);
+    }
+
 }
