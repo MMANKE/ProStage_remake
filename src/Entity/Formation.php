f6b6e81179c2b9f7b7e081e1c9d9265120f37b76
ElCubiko <54866672+ElCubiko@users.noreply.github.com>
2020-02-28 17:00:40 +0100

    Ajout de bootstrap, d'une nav bar et d'un footer + création des entités Entreprise et Formation

---
+++ src/Entity/Formation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=800, nullable=true)
+     */
+    private $nomComplet;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNomComplet(): ?string
+    {
+        return $this->nomComplet;
+    }
+
+    public function setNomComplet(?string $nomComplet): self
+    {
+        $this->nomComplet = $nomComplet;
+
+        return $this;
+    }
+}
